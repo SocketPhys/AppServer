ee6d87154a5ec12b77c64b2792648f87c50b029c
Adithya Mohan <SocketPhys@gmail.com>
2016-06-23 19:48:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var array =[];
+array = {
+    'dubnation':[]
 
-var sendNotification = require('./utils/sendNotification.js');
-
+};
+//var sendNotification = require('./utils/sendNotification.js');
+var request = require('request');
 var app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 // use mongo in production
 var db = {}; // format -> city: [token, token...]
-
+var tweetArray ={};
 // store tokens under appropriate city in db
 app.post('/register', function(req, res) {
   var token = req.body.token;
@@ -21,7 +28,11 @@ app.post('/register', function(req, res) {
   res.sendStatus(200);
 });
 
-// send notification to registered users of a city
+  
+  
+  
+  
+  // send notification to registered users of a city
 app.post('/notification', function(req, res) {
   var notificationData = req.body.data;
   var city = req.body.city;
@@ -34,6 +45,47 @@ app.post('/notification', function(req, res) {
   });
 });
 
+
+app.get('/sendTweets',function(req,res){
+
+    var hashtag = req.query.hashtag;
+    res.send(array[hashtag]);
+
+
+
+});
+
+
+function getTweets(){
+
+    request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded', "Authorization": 'Basic WmR5Tk9tMHJQUlV3VVlwZm1iRDR3eTRCeTpFdVRoZ2FPSEZpNDJsM0h6azlYWjJwaWNCaFA2SFh0RnBDdkNycDN2Zk1zM29TV2NGYw=='},
+        url:     'https://api.twitter.com/oauth2/token',
+        body: 'grant_type=client_credentials'
+    }, function(error, response, body){
+        var token =JSON.parse(body)['access_token'];
+        request.get({
+            headers: {'content-type': 'application/json', 'Authorization':'Bearer ' + token},
+            url: 'https://api.twitter.com/1.1/search/tweets.json?q=%23dubnations'
+
+        },function(e,r,b){
+            var json= JSON.parse(b);
+            array = {
+                'dubnation':[]
+
+            };
+            for(i=0;i<json['statuses'].length;i++){
+                var id=json['statuses'][i]['id_str'];
+                array.dubnation.push("https://twitter.com/statuses/" + id);
+
+            }
+        });
+    });
+
+}
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
+  getTweets();
+  setInterval(function(){ getTweets(); }, 30000);
 });
+
